fb3b59148d54c48b7bd23479334518308fa32d87
Radenz <radenrifqirahman@gmail.com>
2024-04-03 14:47:15 +0700

    feat: scan progress bar prototype

--- Assets/Scripts/Scannable.cs
+++ Assets/Scripts/Scannable.cs
@@ -3,10 +3,24 @@ using UnityEngine;
 public class Scannable : MonoBehaviour
 {
 
+    [SerializeField]
+    private ScanProgressBar _scanProgressBarPrefab;
+
+    private GameObject ScanProgressBarObject => _scanProgressBarPrefab.gameObject;
     private GameObject _scanProgressBar;
 
-    private void Start()
+    public void StartScan()
     {
+        if (_scanProgressBar != null)
+        {
+            _scanProgressBar.SetActive(true);
+            return;
+        }
+        _scanProgressBar = Instantiate(ScanProgressBarObject, transform);
+    }
 
+    public void StopScan()
+    {
+        _scanProgressBar.SetActive(false);
     }
 }
--- Assets/Scripts/Scanner.cs
+++ Assets/Scripts/Scanner.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Scanner : MonoBehaviour
@@ -11,13 +13,16 @@ public class Scanner : MonoBehaviour
     private Transform _transform;
 
     private Transform _objectOnScan;
-    [SerializeField]
-    private float _objectOnScanDistance;
+
+    private LineRenderer _lineOfSight;
+    private List<Transform> _intersectedScannables = new();
+
 
     private void Start()
     {
         _camera = Camera.main;
         _transform = transform;
+        _lineOfSight = transform.GetChild(0).GetComponent<LineRenderer>();
     }
 
     private void Update()
@@ -25,50 +30,97 @@ public class Scanner : MonoBehaviour
         Vector2 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
         Vector2 scannerPosition = _transform.position;
         _direction = (mousePosition - scannerPosition).normalized;
+
+        Vector3 position = _direction * 2.5f;
+        _lineOfSight.SetPosition(1, position);
+    }
+
+    private void FixedUpdate()
+    {
+        ChooseScannable();
+    }
+
+    private void ChooseScannable()
+    {
+        if (_intersectedScannables.Count == 1) return;
+
+        Transform featureToScan = null;
+        float currentOnScanDistance = _objectOnScan == null ? int.MaxValue : DistanceTo(_objectOnScan.position);
+
+        foreach (Transform feature in _intersectedScannables)
+        {
+            if (feature == _objectOnScan) continue;
+
+            if (DistanceTo(feature.position) < currentOnScanDistance)
+                featureToScan = feature;
+        }
+
+        if (featureToScan != null)
+        {
+            Scan(featureToScan);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         Transform transform = other.transform;
-        if (!IsWithinScanRadius(transform.position)) return;
-
-        if (_objectOnScan == null)
+        if (!_intersectedScannables.Contains(transform) && IsWithinScanRadius(transform.position))
         {
-            Debug.Log($"Scanning {transform.gameObject.name}");
-            _objectOnScan = transform;
-            _objectOnScanDistance = DistanceTo(_objectOnScan.position);
-            return;
-        }
-
-        float distance = DistanceTo(transform.position);
-        if (distance < _objectOnScanDistance)
-        {
-            Debug.Log($"Scanning {transform.gameObject.name}");
-            _objectOnScan = transform;
-            _objectOnScanDistance = distance;
+            _intersectedScannables.Add(transform);
+            if (_intersectedScannables.Count == 1) Scan(transform);
         }
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
         Transform transform = other.transform;
-        if (!IsWithinScanRadius(transform.position)) return;
-
-        if (transform == _objectOnScan) return;
-
-        float distance = DistanceTo(transform.position);
-        if (distance < _objectOnScanDistance)
+        if (!_intersectedScannables.Contains(transform) && IsWithinScanRadius(transform.position))
         {
-            Debug.Log($"Scanning {transform.gameObject.name}");
-            _objectOnScan = transform;
-            _objectOnScanDistance = distance;
+            _intersectedScannables.Add(transform);
+            if (_intersectedScannables.Count == 1) Scan(transform);
         }
+
+        if (_intersectedScannables.Contains(transform) && !IsWithinScanRadius(transform.position))
+        {
+            _intersectedScannables.Remove(transform);
+            if (_objectOnScan == transform)
+                StopScanCurrent();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        _intersectedScannables.Remove(other.transform);
+    }
+
+    private void Scan(Transform feature)
+    {
+        // TODO: cache scannable
+        if (_objectOnScan != null)
+        {
+            Scannable currentScannable = _objectOnScan.GetComponent<Scannable>();
+            currentScannable.StopScan();
+        }
+
+        Debug.Log($"Scanning {feature.gameObject.name}");
+        _objectOnScan = feature;
+        // _objectOnScanDistance = DistanceTo(feature.position);
+        Scannable scannable = feature.GetComponent<Scannable>();
+        scannable.StartScan();
+    }
+
+    private void StopScanCurrent()
+    {
+        Scannable scannable = _objectOnScan.GetComponent<Scannable>();
+        scannable.StopScan();
+        _objectOnScan = null;
+        _intersectedScannables.Remove(_objectOnScan);
     }
 
     private float DistanceTo(Vector3 position)
     {
-        // return Vector3.Cross(_direction, position - _transform.position).magnitude;
-        return Vector3.Distance(_transform.position, position);
+        return Vector3.Cross(_direction, position - _transform.position).magnitude;
+        // return Vector3.Distance(_transform.position, position);
     }
 
     private bool IsWithinScanRadius(Vector3 position)
