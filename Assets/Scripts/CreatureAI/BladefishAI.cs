b652adc11a4de7121d91fe18e7967bd85dfe954c
Radenz <radenrifqirahman@gmail.com>
2024-05-14 18:23:54 +0700

    feat, refactor: bladefish AI, refactor creatures AI

--- Assets/Scripts/CreatureAI/BladefishAI.cs
+++ Assets/Scripts/CreatureAI/BladefishAI.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
+using DG.Tweening;
 using Pathfinding;
 using UnityEngine;
 
 // TODO: smoothen bite & run speed
-public class SwordfishAI : MonoBehaviour
+public class BladefishAI : MonoBehaviour
 {
     [SerializeField]
     private Rigidbody2D _rigidbody;
@@ -14,12 +16,15 @@ public class SwordfishAI : MonoBehaviour
     private RangeTrigger _aggresionRange;
     [SerializeField]
     private RangeTrigger _attackHitboxTrigger;
+    [SerializeField]
+    private RangeTrigger _bladeTrigger;
 
     [SerializeField]
     private float _roamingRadius;
     [SerializeField]
     private float _retreatRadius;
     private float _aggresionRadius;
+    private bool _swinging = false;
 
     [SerializeField]
     private float _speed;
@@ -30,10 +35,10 @@ public class SwordfishAI : MonoBehaviour
     private AIPath _ai;
     private float _lastMaxSpeed;
 
-    private SwordfishState _state = SwordfishState.Roaming;
+    private BladefishState _state = BladefishState.Roaming;
     private Transform _transform;
     private Transform _player;
-    private SwordfishState State
+    private BladefishState State
     {
         get => _state;
         set
@@ -52,6 +57,7 @@ public class SwordfishAI : MonoBehaviour
         _aggresionRadius = _aggresionRange.GetComponent<CircleCollider2D>().radius;
         _attackHitboxTrigger.Entered += OnAttack;
         _attackHitboxTrigger.Stay += OnAttack;
+        _bladeTrigger.Entered += OnSwingHit;
 
         // TODO: refactor, use singleton player
         _player = GameObject.FindGameObjectWithTag("Player").transform;
@@ -60,6 +66,11 @@ public class SwordfishAI : MonoBehaviour
         _ai.destination = _transform.RandomWithinRadius(_roamingRadius);
         EventManager.Instance.CreaturesDisabled += DisableAI;
         EventManager.Instance.CreaturesEnabled += EnableAI;
+    }
+
+    private void OnSwingHit()
+    {
+        throw new NotImplementedException();
     }
 
     private void DisableAI()
@@ -77,14 +88,11 @@ public class SwordfishAI : MonoBehaviour
     {
         switch (State)
         {
-            case SwordfishState.Roaming:
+            case BladefishState.Roaming:
                 OnRoaming();
                 break;
-            case SwordfishState.Attacking:
+            case BladefishState.Attacking:
                 OnAttacking();
-                break;
-            case SwordfishState.Retreating:
-                OnRetreating();
                 break;
         }
     }
@@ -105,64 +113,64 @@ public class SwordfishAI : MonoBehaviour
 
     private void OnAttacking()
     {
+        if (_swinging) return;
         _ai.destination = _player.position;
     }
 
     private void OnAttack()
     {
-        if (State != SwordfishState.Attacking) return;
-        PlayerController.Instance.Damage(15);
-        State = SwordfishState.Retreating;
+        if (State != BladefishState.Attacking) return;
+        Swing();
     }
 
-    private void OnRetreating()
+    private void Swing()
     {
-        if (_ai.IsIdle())
-        {
-            if (Vector2.Distance(_transform.position, _player.position) <= _aggresionRadius)
-            {
-                State = SwordfishState.Attacking;
-            }
-            else
-            {
-                State = SwordfishState.Roaming;
-            }
+        _swinging = false;
+        Vector3 angles = _bladeTrigger.transform.localEulerAngles;
+        angles.z = 45;
+        _bladeTrigger.transform.localEulerAngles = angles;
+        _swinging = true;
+        angles.z = -45;
+        _bladeTrigger.transform.DOLocalRotate(angles, 1f).OnComplete(AfterSwing);
+    }
 
-        }
+    private void AfterSwing()
+    {
+        _swinging = false;
+        if (Vector2.Distance(_transform.position, _player.position) <= _aggresionRadius)
+            State = BladefishState.Attacking;
+        else
+            State = BladefishState.Roaming;
     }
 
     private void OnStateChanged()
     {
         switch (State)
         {
-            case SwordfishState.Roaming:
+            case BladefishState.Roaming:
                 _ai.destination = _transform.RandomWithinRadius(_roamingRadius);
                 _ai.maxSpeed = _speed;
                 break;
-            case SwordfishState.Attacking:
+            case BladefishState.Attacking:
                 _ai.destination = _player.position;
                 _ai.maxSpeed = _aggresiveSpeed;
-                break;
-            case SwordfishState.Retreating:
-                _ai.destination = _transform.RandomOnRadius(_retreatRadius);
                 break;
         }
     }
 
     private void OnDetectPlayer()
     {
-        State = SwordfishState.Attacking;
+        State = BladefishState.Attacking;
     }
 
     private void OnLosePlayer()
     {
-        State = SwordfishState.Roaming;
+        State = BladefishState.Roaming;
     }
 }
 
-public enum SwordfishState
+public enum BladefishState
 {
     Roaming,
-    Attacking,
-    Retreating
+    Attacking
 }
