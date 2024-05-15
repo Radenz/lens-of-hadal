a6051885096957a35bbff2628d217a70353f6b2f
Radenz <radenrifqirahman@gmail.com>
2024-05-15 14:47:08 +0700

    feat: bgm & player sprite

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,60 @@
+using DG.Tweening;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    [SerializeField]
+    private AudioSource _bgmBackgroundLayer;
+
+    [SerializeField]
+    private AudioSource _bgmBossfightLayer;
+
+    [SerializeField]
+    private float _crossfadeDuration = 2f;
+
+    private void Start()
+    {
+        PlayBgm();
+    }
+
+    public void PlayBgm()
+    {
+        _bgmBackgroundLayer.Play();
+        _bgmBossfightLayer.Play();
+        _bgmBossfightLayer.volume = 0;
+        _bgmBackgroundLayer.loop = true;
+    }
+
+    [Button]
+    public void PlayBossfightLayer()
+    {
+        DOTween.To(
+            () => _bgmBossfightLayer.volume,
+            volume => _bgmBossfightLayer.volume = volume,
+            1f,
+            _crossfadeDuration
+        )
+            .OnUpdate(() =>
+            {
+                _bgmBackgroundLayer.volume = 1 - _bgmBossfightLayer.volume;
+            })
+            .SetEase(Ease.Linear);
+    }
+
+    [Button]
+    public void StopBossfightLayer()
+    {
+        DOTween.To(
+            () => _bgmBackgroundLayer.volume,
+            volume => _bgmBackgroundLayer.volume = volume,
+            1f,
+            _crossfadeDuration
+        )
+            .OnUpdate(() =>
+            {
+                _bgmBossfightLayer.volume = 1 - _bgmBackgroundLayer.volume;
+            })
+            .SetEase(Ease.Linear);
+    }
+}
