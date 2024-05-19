0b5d56ed6d183176232166acfc6ca9a7d6ef0212
Radenz <radenrifqirahman@gmail.com>
2024-05-19 16:47:40 +0700

    fix: quest display hide & show

--- Assets/Scripts/Core/Quest/QuestHUD.cs
+++ Assets/Scripts/Core/Quest/QuestHUD.cs
@@ -11,21 +11,25 @@ public class QuestHUD : MonoBehaviour
 
     private ManagedTween _tween = new();
 
+    private RectTransform _transform;
+
     private void Start()
     {
+        _transform = (RectTransform)transform;
+
         EventManager.Instance.DisplayQuestChanged += UpdateQuestDisplay;
         EventManager.Instance.DisplayQuestHidden += HideDisplayQuest;
     }
 
     private void UpdateQuestDisplay(string title, string description)
     {
-        _tween.PlayNext(transform.DOLocalMoveX(-Mathf.Abs(transform.localPosition.x), 0.2f));
+        _tween.PlayNext(transform.DOLocalMoveX(0, 0.2f));
         _title.text = title;
         _description.text = description;
     }
 
     private void HideDisplayQuest()
     {
-        _tween.PlayNext(transform.DOLocalMoveX(Mathf.Abs(transform.localPosition.x), 0.2f));
+        _tween.PlayNext(transform.DOLocalMoveX(_transform.rect.width, 0.2f));
     }
 }
