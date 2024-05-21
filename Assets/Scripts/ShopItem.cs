4c0a5a924a88efaf9330133d579750a7aafac548
Radenz <radenrifqirahman@gmail.com>
2024-05-21 22:48:20 +0700

    feat: set shop item image on start

--- Assets/Scripts/ShopItem.cs
+++ Assets/Scripts/ShopItem.cs
@@ -2,12 +2,19 @@ using Common.Persistence;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ShopItem : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField]
     private Item _item;
     public Item Item => _item;
+
+    private void Start()
+    {
+        Image image = GetComponent<Image>();
+        image.sprite = _item.Sprite;
+    }
 
     public void OnPointerClick(PointerEventData eventData)
     {
